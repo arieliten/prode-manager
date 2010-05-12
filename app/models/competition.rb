150aae7485105e6b4a14366f52d6889b70d17927
Ariel Fernando Diaz Bermejo <arieldiazbermejo@gmail.com>
2010-05-12 17:12:51 -0300

    Fixing relation problem and commenting validations on Cup model

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,7 +2,7 @@ class Competition < ActiveRecord::Base
   # == Relations
   has_many :competition_stages
   has_many :matches, :through=>:competition_stages, :source=>:matches
-  has_many :teams, :through=>:competition_teams, :source=>:team
+  has_many :teams
 
   # == Validations
   validates_presence_of :name, :max_number_of_participants
